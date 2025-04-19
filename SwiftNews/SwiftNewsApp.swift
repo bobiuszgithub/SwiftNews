21551d6ded5860de1d8b5e557a15ee5e370ea67e
Cs Bence <bcsaldi18@gmail.com>
2025-04-19 15:00:27 +0200

    Initial Commit

---
+++ SwiftNews/SwiftNewsApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftNewsApp.swift
+//  SwiftNews
+//
+//  Created by Bence Családi on 2025. 04. 19..
+//
+
+import SwiftUI
+
+@main
+struct SwiftNewsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
